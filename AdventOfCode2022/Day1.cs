775f12461b90133a1ca2afa8d27574b60450c51f
Johan Svensson <Johan.Svensson@strongpoint.com>
2022-12-01 11:20:35 +0100

    Add project files.

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode2022;
+
+internal class Day1
+{
+    private static List<int> CaloriesByElf()
+    {
+        return File.ReadAllLines(@"..\..\..\Day1Input.txt").Aggregate(new List<int> { 0 },
+                                           (list, value) =>
+                                           {
+                                               if (!string.IsNullOrEmpty(value))
+                                                   list[list.Count - 1] += int.Parse(value);
+                                               else
+                                                   list.Add(0);
+                                               return list;
+                                           });
+    }
+
+    public static int ExecutePart1()
+    {
+        return CaloriesByElf().Max();
+    }
+
+    public static int ExecutePart2()
+    {
+        return CaloriesByElf().OrderDescending().Take(3).Sum();
+    }
+}
