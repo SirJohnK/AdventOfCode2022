a0cd26a279ca2f23af227703cae7baf4242c9126
Johan Svensson <Johan.Svensson@strongpoint.com>
2022-12-04 13:33:32 +0100

    Day 4 solution

---
+++ AdventOfCode2022/Day4.cs
@@ -0,0 +1,27 @@
+﻿using static System.Collections.Specialized.BitVector32;
+
+namespace AdventOfCode2022;
+
+internal class Day4
+{
+    public static int ExecutePart1()
+    {
+        return File.ReadAllLines(@"..\..\..\Day4Input.txt")
+            .Select(line => line.Split(","))
+            .Select(assignments => (assignments[0].Split("-"), assignments[1].Split("-")))
+            .Select(ranges => (int.Parse(ranges.Item1[0]), int.Parse(ranges.Item1[1]), int.Parse(ranges.Item2[0]), int.Parse(ranges.Item2[1])))
+            .Count(sections => (sections.Item1 <= sections.Item3 && sections.Item2 >= sections.Item4)
+                               || (sections.Item3 <= sections.Item1 && sections.Item4 >= sections.Item2));
+    }
+
+    public static int ExecutePart2()
+    {
+        return File.ReadAllLines(@"..\..\..\Day4Input.txt")
+            .Select(line => line.Split(","))
+            .Select(assignments => (assignments[0].Split("-"), assignments[1].Split("-")))
+            .Select(ranges => (int.Parse(ranges.Item1[0]), int.Parse(ranges.Item1[1]), int.Parse(ranges.Item2[0]), int.Parse(ranges.Item2[1])))
+            .Count(sections => Enumerable.Range(sections.Item1, (sections.Item2 - sections.Item1) + 1)
+                               .Intersect(Enumerable.Range(sections.Item3, (sections.Item4 - sections.Item3) + 1))
+                               .Any());
+    }
+}
